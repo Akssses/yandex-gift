4c5f8f9fb06521d8abaa5c121f4e7d83c3bcb5e2
Akssses <itshnik07@gmail.com>
2025-12-17 07:23:07 +0600

    fixed

--- server/bot/management/commands/broadcast_new_gift.py
+++ server/bot/management/commands/broadcast_new_gift.py
@@ -16,7 +16,7 @@ from telegram import Bot, InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
 from bot.models import TelegramUser
 
 
-MESSAGE_TEXT = ("Подарков много не бывает. Поэтому мы подготовили ещё больше промокодов к Новому году 🎄")
+MESSAGE_TEXT = ("Продолжаем! Новый подарок уже доступен.")
 
 
 
